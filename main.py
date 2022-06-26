796b428150c6d4ebcbe60db5d7e6bd2ed35cd52e
Lorenzo Murillo <100735769+LorenzoNM@users.noreply.github.com>
2022-06-26 20:25:11 +0000

    Comp 1 Trial 2 PLay, Exit Buttons

--- main.py
+++ main.py
@@ -8,21 +8,25 @@ class TitlePage:
    background_color="Grey"
  
    self.bg_image = Image.open("2.png")
-   self.bg_image = self.bg_image.resize((515,515), Image.ANTIALIAS)
+   self.bg_image = self.bg_image.resize((475,475), Image.ANTIALIAS)
    self.bg_image = ImageTk.PhotoImage(self.bg_image) 
-   self.title_frame=Frame(parent, bg = background_color, padx=200, pady=220)
+   self.title_frame=Frame(parent, bg = background_color, padx=180, pady=200)
    self.title_frame.grid()         
         
    self.bg_frame= Label(self.title_frame, image=self.bg_image)
-   self.bg_frame.place(x=-305, y=-220, relwidth=6, relheight=6.8) 
+   self.bg_frame.place(x=-205, y=-190, relwidth=3, relheight=7) 
     
-   self.play_button = Button(self.title_frame, text="PLAY", font=("Helvetica", "13", "bold"), bg="green")
-   self.play_button.grid(row=3, padx=20, pady=20)        
+   self.play_button = Button(self.title_frame, text="PLAY", font=("Courier", "13", "bold"), bg="green")
+   self.play_button.place(x=170, y=170)
 
-
+   self.exit_button = Button(self.title_frame, text="EXIT", font=("Courier", "13", "bold"), bg="Red")
+   self.exit_button.place(x=-30, y=170)
+   
+   self.entry_box=Entry(self.title_frame)
+   self.entry_box.grid(row=2,padx=20, pady=20)
 
 if __name__ == "__main__":
    system = Tk()
-   system.title("NZ Road Rules Quiz")
+   system.title("Shooting Game Quiz")
    quiz_instance = TitlePage(system)
    system.mainloop()
