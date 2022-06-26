2a54bd73cbe451f7f16b50e0b45854e1cfbf2ca4
Lorenzo Murillo <100735769+LorenzoNM@users.noreply.github.com>
2022-06-26 14:15:07 +0000

    Comp 1 Trial 1.2 Size Correction

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ class TitlePage:
    self.bg_image = Image.open("2.png")
    self.bg_image = self.bg_image.resize((515,515), Image.ANTIALIAS)
    self.bg_image = ImageTk.PhotoImage(self.bg_image) 
-   self.title_frame=Frame(parent, bg = background_color, padx=180, pady=220)
+   self.title_frame=Frame(parent, bg = background_color, padx=200, pady=220)
    self.title_frame.grid()         
         
    self.bg_frame= Label(self.title_frame, image=self.bg_image)
-   self.bg_frame.place(x=-185, y=-220, relwidth=3.3, relheight=6.8) 
+   self.bg_frame.place(x=-305, y=-220, relwidth=6, relheight=6.8) 
     
    self.play_button = Button(self.title_frame, text="PLAY", font=("Helvetica", "13", "bold"), bg="green")
    self.play_button.grid(row=3, padx=20, pady=20)        
