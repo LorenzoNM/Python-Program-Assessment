54f6490e1a4139f17976eff432eacebaf0384410
Lorenzo Murillo <100735769+LorenzoNM@users.noreply.github.com>
2022-07-03 21:54:25 +0000

    Comp 1 name collection, username box, play and exit buttons and start of Comp 2

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 from tkinter import *
 from PIL import Image, ImageTk
-
-names = []
+name = []
 class TitlePage:
  def __init__(self, parent):
 
-   background_color="Grey"
- 
-   self.bg_image = Image.open("2.png")
-   self.bg_image = self.bg_image.resize((475,475), Image.ANTIALIAS)
-   self.bg_image = ImageTk.PhotoImage(self.bg_image) 
-   self.title_frame=Frame(parent, bg = background_color, padx=180, pady=200)
-   self.title_frame.grid()         
-        
-   self.bg_frame= Label(self.title_frame, image=self.bg_image)
-   self.bg_frame.place(x=-205, y=-190, relwidth=3, relheight=7) 
-    
-   self.play_button = Button(self.title_frame, text="PLAY", font=("Courier", "13", "bold"), bg="green")
-   self.play_button.place(x=170, y=170)
+   background_color="DarkBlue"
+  
+   self.play_button = Button(parent, text="PLAY", font=("Courier", "13", "bold"),bg = "Green",command=self.name_collection)
+   self.play_button.place(x=280, y=375,height= 50, width=155)
 
-   self.exit_button = Button(self.title_frame, text="EXIT", font=("Courier", "13", "bold"), bg="Red")
-   self.exit_button.place(x=-30, y=170)
+   self.exit_button = Button(parent, text="EXIT", font=("Courier", "13", "bold"), bg="Red")
+   self.exit_button.place(x=65, y=375,height= 50, width=155)
+
+   self.username_box=Entry(parent,borderwidth=2)
+   self.username_box.place(x=171.8,y=297.5,height= 25, width=155)
+ def name_collection(self):
+   name=self.username_box.get()
+   names.append(name)
+   self.quiz_frame.destroy()
+   TitlePage(root)  
+class QuizPage:
+ def __init__(self, parent):
+   background_color=("DarkBlue")
    
-   self.entry_box=Entry(self.title_frame)
-   self.entry_box.grid(row=2,padx=20, pady=20)
-
 if __name__ == "__main__":
    system = Tk()
+   system.geometry("500x500")
+
    system.title("Shooting Game Quiz")
-   quiz_instance = TitlePage(system)
+   bg_image = Image.open("2.png")
+   bg_image = bg_image.resize((500,500), Image.ANTIALIAS)
+   bg_image = ImageTk.PhotoImage(bg_image) 
+   bg_frame= Label(system, image=bg_image)
+   bg_frame.place(x=0, y=2, relwidth=1, relheight=1)
+   
+   quiz_program = TitlePage(system)
    system.mainloop()
